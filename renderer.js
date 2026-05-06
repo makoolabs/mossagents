aab28322a919e822679f9d28cc594160881d59cb
coffis <mossgame@163.com>
2026-05-07 07:27:18 +0800

    test

---
+++ renderer.js
@@ -0,0 +1,7 @@
+const information = document.getElementById('info')
+information.innerText = `本应用正在使用 Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), 和 Electron (v${window.versions.electron()})`
+const func = async () => {
+  const response = await window.versions.ping()
+  console.log(response) // prints "pong"
+}
+func()
